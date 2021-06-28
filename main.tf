55d723114b9d81a5d58197cca90d56a83c07839f
adam.schaible <adam.schaible@yodlee.com>
2021-06-28 13:06:46 -0400

    add test instance

--- main.tf
+++ main.tf
@@ -21,3 +21,10 @@ resource "aws_vpc" "schibes-demo-vpc" {
     cidr_block = "10.70.0.0/16"
     enable_dns_hostnames = "true"
 }
+
+# Create an EC2 instance
+resource "aws_instance" "server1" {
+    ami = "ami-02e30ba14d8ffa6e6" #Ubuntu 16 in us-east-2
+    instance_type = "t2.micro"
+    associate_public_ip_address = "true"
+}
